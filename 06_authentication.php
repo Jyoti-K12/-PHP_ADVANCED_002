7c17df2fa70582d7cdb4da42cc85ce9f5bbd8f83
Jyoti Kumari Ranote <jyotiranote97@gmail.com>
2024-02-25 22:56:33 +0530

    authentication

---
+++ 06_authentication.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * ================================================================================================
+ * 1. what is the difference between authentication and authorization?
+ * ================================================================================================
+ */
+
+/*
+ Authentication
+ ==================
+
+ 1. Authentication verifies who the user is.
+ 2. Authentication works through passwords, one-time pins, biometric information, and other information provided or entered by the user.
+ 3. Authentication is the first step of a good identity and access management process.
+ 4. Authentication is visible and changeable by the user.
+*/
+
+/*
+ Authorization
+ ==================
+
+ 1. Authorization determines what resources a user can access.
+ 2. Authorization works through settings that are implemented and maintained by the organization.
+ 3. Authorization always takes place after authentication.
+ 4. Authorization isn’t visible to or changeable by the user.
+*/
+
+
+
+
+/**
+ * ================================================================================================
+ * 2. What are some common authentication methods used in web applications?
+ * ================================================================================================
+ */
+/*
+ - Username and Password Authentication:
+ - Token-based Authentication: (JWT, Passport and custom token)
+ - OAuth Authentication: (manage third party authentication)
+ - Biometric Authentication: (fingerprints, facial recognition)
+ - Certificate-based Authentication: (certificate that issued by admin)
+ - Social Media Authentication: (such as Google, Facebook, or Twitter)
+*/
